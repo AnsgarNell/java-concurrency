6dba90c8e957149716977c59470daa8ff37adf51
Ansgar Nell <ansgar.nell@hiq.de>
2026-08-20 11:08:07 -0300

    main: added callable class which returns a string value, and a test for it.

--- src/test/java/com/nellpy/executors/ExecutorsTest.java
+++ src/test/java/com/nellpy/executors/ExecutorsTest.java
@@ -3,8 +3,10 @@ package com.nellpy.executors;
 import com.nellpy.threads.HelloThread;
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 
 public class ExecutorsTest {
@@ -13,6 +15,16 @@ public class ExecutorsTest {
     public void singleThreadExecutor() {
         try(ExecutorService executorService = Executors.newSingleThreadExecutor()) {
             executorService.execute(new HelloThread());
+        }
+    }
+
+
+    @Test
+    public void callable() throws ExecutionException, InterruptedException {
+        try(ExecutorService executorService = Executors.newSingleThreadExecutor()) {
+            Future<String> future = executorService.submit(new HelloStringCallable());
+            String result = future.get();
+            System.out.println(result);
         }
     }
 
