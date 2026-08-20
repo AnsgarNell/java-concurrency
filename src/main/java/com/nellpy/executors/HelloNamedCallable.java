2252a9e031e83a4168ab62b9d1c475d952039ae1
Ansgar Nell <ansgar.nell@hiq.de>
2026-08-20 11:13:24 -0300

    main: ExecutorService invokeAll example with single thread executor.

---
+++ src/main/java/com/nellpy/executors/HelloNamedCallable.java
@@ -0,0 +1,13 @@
+package com.nellpy.executors;
+
+import java.util.concurrent.Callable;
+
+
+public class HelloNamedCallable implements Callable<String> {
+
+    @Override
+    public String call() {
+        return "Hello from callable thread " + Thread.currentThread().getName() + "!";
+    }
+
+}
--- src/test/java/com/nellpy/executors/ExecutorsTest.java
+++ src/test/java/com/nellpy/executors/ExecutorsTest.java
@@ -3,6 +3,9 @@ package com.nellpy.executors;
 import com.nellpy.threads.HelloThread;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -26,6 +29,27 @@ public class ExecutorsTest {
             String result = future.get();
             System.out.println(result);
         }
+    }
+
+
+    @Test
+    public void invokeAllWithSingleThreadExecutor() throws InterruptedException, ExecutionException {
+        try(ExecutorService executorService = Executors.newSingleThreadExecutor()) {
+            List<Future<String>> futures = executorService.invokeAll(getCallables());
+            for (Future<String> future : futures) {
+                String result = future.get();
+                System.out.println(result);
+            }
+        }
+    }
+
+
+    private static Collection<Callable<String>> getCallables() {
+        return List.of(
+                new HelloNamedCallable(),
+                new HelloNamedCallable(),
+                new HelloNamedCallable()
+        );
     }
 
 }
