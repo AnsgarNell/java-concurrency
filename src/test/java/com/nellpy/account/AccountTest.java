8f304a6c479aea005f2b141c6a41cf0e49a4dcad
Ansgar Nell <ansgar.nell@hiq.de>
2026-08-20 12:50:36 -0300

    main: added Account class and tests for thread interference.

---
+++ src/test/java/com/nellpy/account/AccountTest.java
@@ -0,0 +1,30 @@
+package com.nellpy.account;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static com.nellpy.account.Account.INITIAL_BALANCE;
+
+
+class AccountTest {
+
+    public static final int N_THREADS = 10;
+
+
+    @Test
+    public void testThreadInterference() {
+        Account account = new Account();
+        Assertions.assertEquals(INITIAL_BALANCE, account.value());
+        try(ExecutorService executorService = Executors.newFixedThreadPool(N_THREADS)) {
+            for (int i = 0; i < N_THREADS; i++) {
+                executorService.execute(account::increment);
+                executorService.execute(account::decrement);
+            }
+        }
+        Assertions.assertEquals(INITIAL_BALANCE, account.value());
+    }
+
+}
