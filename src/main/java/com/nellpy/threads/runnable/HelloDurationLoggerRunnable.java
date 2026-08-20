3568acc37269d5305265081095fbc81797c79b13
Ansgar Nell <ansgar.nell@hiq.de>
2026-08-20 12:46:00 -0300

    main: added HelloDurationLoggerRunnable class and tests for different pools.

---
+++ src/main/java/com/nellpy/threads/runnable/HelloDurationLoggerRunnable.java
@@ -0,0 +1,23 @@
+package com.nellpy.threads.runnable;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.concurrent.TimeUnit;
+
+
+public class HelloDurationLoggerRunnable implements Runnable {
+
+    @Override
+    public void run() {
+        Instant start = Instant.now();
+        try {
+            TimeUnit.MILLISECONDS.sleep(100);
+        } catch (InterruptedException ie) {
+            Thread.currentThread().interrupt();
+        }
+        Instant finish = Instant.now();
+        long timeElapsed = Duration.between(start, finish).toMillis();
+        System.out.println("Time elapsed: " + timeElapsed + " ms");
+    }
+
+}
--- src/test/java/com/nellpy/threads/virtual/VirtualThreadsTest.java
+++ src/test/java/com/nellpy/threads/virtual/VirtualThreadsTest.java
@@ -1,7 +1,12 @@
 package com.nellpy.threads.virtual;
 
+import com.nellpy.threads.runnable.HelloDurationLoggerRunnable;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.LockSupport;
 
@@ -25,6 +30,50 @@ public class VirtualThreadsTest {
             System.out.println("Reached thread limit: " + threadCount);
             System.err.println(error.getMessage());
             System.exit(1);
+        }
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(ints = {1_000})
+    public void newWorkStealingPool(int numberOfThreads) {
+        try(ExecutorService executorService = Executors.newWorkStealingPool()) {
+            for (int i = 0; i < numberOfThreads; i++) {
+                executorService.execute(new HelloDurationLoggerRunnable());
+            }
+        }
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(ints = {1_000, 2_000, 10_000})
+    public void newFixedThreadPool(int numberOfThreads) {
+        try(ExecutorService executorService = Executors.newFixedThreadPool(numberOfThreads)) {
+            for (int i = 0; i < numberOfThreads; i++) {
+                executorService.execute(new HelloDurationLoggerRunnable());
+            }
+        }
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(ints = {1_000, 2_000, 10_000, 100_000})
+    public void newCachedThreadPool(int numberOfThreads) {
+        try(ExecutorService executorService = Executors.newCachedThreadPool()) {
+            for (int i = 0; i < numberOfThreads; i++) {
+                executorService.execute(new HelloDurationLoggerRunnable());
+            }
+        }
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(ints = {1_000, 2_000, 10_000, 100_000, 1_000_000})
+    public void newVirtualThreadPerTaskExecutor(int numberOfThreads) {
+        try(ExecutorService executorService = Executors.newVirtualThreadPerTaskExecutor()) {
+            for (int i = 0; i < numberOfThreads; i++) {
+                executorService.execute(new HelloDurationLoggerRunnable());
+            }
         }
     }
 
